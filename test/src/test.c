7ecc6307cc47bb6ba118da6f3b146493225df7bf
warlf <261584342@qq.com>
2018-08-28 16:57:41 +0800

    0.0

--- test/src/test.c
+++ test/src/test.c
@@ -8,11 +8,18 @@
  ============================================================================
  */
 
-#include <stdio.h>
+#include<stdio.h>
 
 int main()
 {
-	printf("* * * * * * * * * * * * * * * * * * * *");
-	printf("               Very good!              ");
-	printf("* * * * * * * * * * * * * * * * * * * *");
+	int i,max,min,n;
+	scanf("%d",&n);
+	max=n;min=n;
+	for(i=1;i<10;i++)
+	{
+		scanf("%d",&n);
+		if(max<n) max=n;
+		if(min>n) min=n;
+	}
+	printf("最大数:%d,最小数:%d",max,min);
 }
